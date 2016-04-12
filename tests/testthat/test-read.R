aff783cbe0af65fc893864c0b3bd251ed9c63d4b
Michael Sumner <Michael.Sumner@aad.gov.au>
2016-04-12 15:53:06 +1000

    test read - tokens are stable, verts are parsed

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,5 +1,7 @@
 context("reading bgm")
 
+
+## extract the number in a 
 namenumber <- function(x) {
   x <- unlist(lapply(strsplit(x, "\\."), "[", 1))
   as.integer(gsub("[[:alpha:]]|[[:punct:]]", "", x))
@@ -8,6 +10,25 @@ removetrailing <- function(x) gsub("\\s+$", "", x)
 removeleading <- function(x) gsub("^\\s+", "", x)
 singlespace <- function(x) gsub("\\s+", " ", x)
 addnewline <- function(x) sprintf("%s\n", x)
+spaceseparate1 <- function(x) sort(unique(unlist(lapply(strsplit(singlespace(removetrailing(removeleading(x))), " "), function(a) gsub("[[:digit:]]", "", a[1L])))))
+dotvertlines <- function(x, type, app) {
+  grep(sprintf("^%s[0-9]+.%s\\s", type, app), x, value = TRUE)
+}
+vertlines <- function(x, type) {
+  grep(sprintf("^%s", type), x, value = TRUE)
+}
+verts <- function(x, type, app = "") {
+  x1 <- if (app == "") {vertlines(x, type)}  else {x <- dotvertlines(x, type, app)}
+  x2 <- singlespace(x1)
+  x3 <- removetrailing(x2)
+  x4 <- removeleading(x3)
+  x5 <- addnewline(x4)
+  
+  x6 <- setNames(read.delim(text = x5, sep= " ", header = FALSE, stringsAsFactors = FALSE), c("name", "x", "y"))
+  if (!app == "") x6$number <- namenumber(x6$name)
+  as_data_frame(x6)
+}
+
 
 library(dplyr)
 files <- bgmfiles::bgmfiles()
@@ -16,42 +37,38 @@ basetoks <- c("#", "bnd_vert", "box.area", "box.botz", "box.horizmix", "box.ibox
               "box.vertmix", "face.cs", "face.length", "face.lr", "face.p",
               "maxwcbotz", "nbox", "nface", "projection")
 
-test_that("known tokens are always the same\n", {
-## this test
-  ## removes leading and trailing whitespace
-  ## converts all whitespace to single spaces (including tabs, not include new lines)
-  ## splits every line in the file on " " and lists the unique first tokens and sorts them
-  ## compares that list to basetoks above
-  ## if this test fails it may mean we have a new kind of file
-  ## with a new bit of information
-  ## or without something the other files have
+
 for (i in seq_along(files)) {
   f <- files[i]
   d <- data_frame(tx = readLines(f))
-  xtoks <- sort(unique(unlist(lapply(strsplit(singlespace(removetrailing(removeleading(d$tx))), " "), function(x) gsub("[[:digit:]]", "", x[1L])))))
+  xtoks <- spaceseparate1(d$tx) 
   ## remove any lines that start with "##A"nyletter 
   bad <- grep("^##[[:alpha:]]", xtoks)
   if (length(bad) > 0)  xtoks <- xtoks[-bad]
   #print(all(xtoks == basetoks))
-  testthat::expect_that(xtoks, testthat::is_identical_to(basetoks))
+  
+  test_that("known tokens are always the same", {
+    ## this test
+    ## removes leading and trailing whitespace
+    ## converts all whitespace to single spaces (including tabs, not include new lines)
+    ## removes any lines that begin with two hashes and a letter "^##[[:alpha:]]"
+    ## splits every line in the file on " " and lists the unique first tokens and sorts them
+    ## compares that list to basetoks above
+    ## if this test fails it may mean we have a new kind of file
+    ## with a new bit of information
+    ## or without something the other files have
+    testthat::expect_that(xtoks, testthat::is_identical_to(basetoks))
+    
+  })
+  
+  test_that("all vertices are read as data frames", {
+    
+    testthat::expect_that(bverts <- verts(d$tx, "box", "vert"), is_a("tbl_df"))
+    testthat::expect_that(f1verts <- verts(d$tx, "face", "p1"), is_a("tbl_df"))
+    testthat::expect_that(f2verts <- verts(d$tx, "face", "p2"), is_a("tbl_df"))
+    testthat::expect_that(bndverts <- verts(d$tx, "bnd_vert", ""), is_a("tbl_df"))
+    
+  })
+  
 }
-})
 
-# 
-# verts <- function(x, type, app) {
-#   x1 <- grep(sprintf("^%s[0-9]+.%s\\s", type, app), x, value = TRUE)
-#   x2 <- singlespace(x1)
-#   x3 <- removetrailing(x2)
-#   x4 <- removeleading(x3)
-#   x5 <- addnewline(x4)
-# 
-#   x6 <- setNames(read.delim(text = x5, sep= " ", header = FALSE, stringsAsFactors = FALSE), c("name", "x", "y"))
-#   x6$number <- namenumber(x6$name)
-#   as_data_frame(x6)
-# }
-# bverts <- verts(d$tx, "box", "vert")
-# f1verts <- verts(d$tx, "face", "p1")
-# f2verts <- verts(d$tx, "face", "p2")
-# 
-# 
-# 
