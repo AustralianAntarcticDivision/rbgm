6b3feecd049e922be30bba4d537d583036633d0b
Michael Sumner <Michael.Sumner@aad.gov.au>
2016-04-12 15:30:45 +1000

    test read - double comments need handling

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,39 +1,43 @@
-# context("reading bgm")
+context("reading bgm")
+
+namenumber <- function(x) {
+  x <- unlist(lapply(strsplit(x, "\\."), "[", 1))
+  as.integer(gsub("[[:alpha:]]|[[:punct:]]", "", x))
+}
+removetrailing <- function(x) gsub("\\s+$", "", x)
+removeleading <- function(x) gsub("^\\s+", "", x)
+singlespace <- function(x) gsub("\\s+", " ", x)
+addnewline <- function(x) sprintf("%s\n", x)
+
+library(dplyr)
+files <- bgmfiles::bgmfiles()
+basetoks <- c("#", "bnd_vert", "box.area", "box.botz", "box.horizmix", "box.ibox",
+              "box.iface", "box.inside", "box.label", "box.nconn", "box.vert",
+              "box.vertmix", "face.cs", "face.length", "face.lr", "face.p",
+              "maxwcbotz", "nbox", "nface", "projection")
+
+test_that("known tokens are always the same\n", {
+## this test
+  ## removes leading and trailing whitespace
+  ## converts all whitespace to single spaces (including tabs, not include new lines)
+  ## splits every line in the file on " " and lists the unique first tokens and sorts them
+  ## compares that list to basetoks above
+  ## if this test fails it may mean we have a new kind of file
+  ## with a new bit of information
+  ## or without something the other files have
+for (i in seq_along(files)) {
+  f <- files[i]
+  d <- data_frame(tx = readLines(f))
+  xtoks <- sort(unique(unlist(lapply(strsplit(singlespace(removetrailing(removeleading(d$tx))), " "), function(x) gsub("[[:digit:]]", "", x[1L])))))
+  ## remove any lines that start with "##A"nyletter 
+  bad <- grep("^##[[:alpha:]]", xtoks)
+  if (length(bad) > 0)  xtoks <- xtoks[-bad]
+  #print(all(xtoks == basetoks))
+  testthat::expect_that(xtoks, testthat::is_identical_to(basetoks))
+}
+})
+
 # 
-# 
-# library(dplyr)
-# files <- bgmfiles::bgmfiles()
-# basetoks <- c("#", "bnd_vert", "box.area", "box.botz", "box.horizmix", "box.ibox", 
-#               "box.iface", "box.inside", "box.label", "box.nconn", "box.vert", 
-#               "box.vertmix", "face.cs", "face.length", "face.lr", "face.p", 
-#               "maxwcbotz", "nbox", "nface", "projection")
-# 
-# test_that("known tokens are always the same", {
-# ## this test 
-#   ## removes leading and trailing whitespace
-#   ## converts all whitespace to single spaces (including tabs, not include new lines)
-#   ## splits every line in the file on " " and lists the unique first tokens and sorts them
-#   ## compares that list to basetoks above
-#   ## if this test fails it may mean we have a new kind of file
-#   ## with a new bit of information
-#   ## or without something the other files have
-# for (i in seq_along(files)) {
-#   f <- files[i]
-#   d <- data_frame(tx = readLines(f))
-#   xtoks <- sort(unique(unlist(lapply(strsplit(singlespace(removetrailing(removeleading(d$tx))), " "), function(x) gsub("[[:digit:]]", "", x[1L])))))
-#   testthat::expect_that(xtoks, testthat::is_identical_to(basetoks))
-# }
-# })
-# 
-# 
-# namenumber <- function(x) {
-#   x <- unlist(lapply(strsplit(x, "\\."), "[", 1))
-#   as.integer(gsub("[[:alpha:]]|[[:punct:]]", "", x))
-# }
-# removetrailing <- function(x) gsub("\\s+$", "", x)
-# removeleading <- function(x) gsub("^\\s+", "", x)
-# singlespace <- function(x) gsub("\\s+", " ", x)
-# addnewline <- function(x) sprintf("%s\n", x)
 # verts <- function(x, type, app) {
 #   x1 <- grep(sprintf("^%s[0-9]+.%s\\s", type, app), x, value = TRUE)
 #   x2 <- singlespace(x1)
