71ef7c81e389e4eb2685026cc1feb029ce02087a
Michael Sumner <mdsumner@gmail.com>
2016-06-17 07:45:32 +1000

    turn off coincidence tests for all but one file

--- tests/testthat/test-round-trip.R
+++ tests/testthat/test-round-trip.R
@@ -5,23 +5,26 @@ context("round-trip of BGM links")
 library(rbgm)
 library(dplyr)
 library(spdplyr)
-bfile <- grep("antarctica_99", bgmfiles::bgmfiles(), value = TRUE)
-bgm <- bgmfile(bfile)
-polybgm <- rbgm::boxSpatial(bgm)
-linebgm <- rbgm::faceSpatial(bgm)
-
-## note the minus one on seq
-for (abox in (seq(nrow(polybgm)) -1)) {
-  aline <- linebgm %>% filter(.fx0 %in% (bgm$facesXboxes %>% dplyr::filter(.bx0 == abox))$iface)
-  poly <- polybgm %>% filter(.bx0 == abox) 
-  plot(poly)
-  plot(aline, add = TRUE, col = "red")
+#bfile <- grep("antarctica_99", bgmfiles::bgmfiles(), value = TRUE)
+#for (bfile in bgmfiles::bgmfiles()) {
+  context(sprintf("reading file %s", basename(bfile)))
+  bgm <- bgmfile(bfile)
+  polybgm <- rbgm::boxSpatial(bgm)
+  linebgm <- rbgm::faceSpatial(bgm)
   
-  #scan("", 1)
-  test_that("round tripping works to get the right box and faces", {
-    expect_true(rgeos::gIntersects(aline, poly))
-    expect_true(rgeos::gCovers(poly, aline))
-    expect_true(rgeos::gCoveredBy(aline, poly))
+  ## note the minus one on seq
+  for (abox in (seq(nrow(polybgm)) -1)) {
+    aline <- linebgm %>% filter(.fx0 %in% (bgm$facesXboxes %>% dplyr::filter(.bx0 == abox))$iface)
+    poly <- polybgm %>% filter(.bx0 == abox) 
+#    plot(poly)
+#    plot(aline, add = TRUE, col = "red")
     
-  })
-}
+    #scan("", 1)
+    test_that("round tripping works to get the right box and faces", {
+      expect_true(rgeos::gIntersects(aline, poly))
+      expect_true(rgeos::gCovers(poly, aline))
+      expect_true(rgeos::gCoveredBy(aline, poly))
+      
+    })
+  }
+#}
