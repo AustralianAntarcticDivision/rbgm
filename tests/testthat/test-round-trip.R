4ccc45d98095823d70e3f29e90a6663b3851aeb2
Michael Sumner <mdsumner@gmail.com>
2016-06-24 16:04:45 +1000

    future test

--- tests/testthat/test-round-trip.R
+++ tests/testthat/test-round-trip.R
@@ -28,3 +28,20 @@ bfile <- grep("antarctica_99", bgmfiles::bgmfiles(), value = TRUE)
     })
   }
 #}
+  
+## run on some systems
+  # library(bgmfiles)
+  # library(rbgm)
+  # bgm <- bgmfile(grep("NGOM", bgmfiles::bgmfiles(), value = TRUE))
+  # 
+  # boxes <- boxSpatial(bgm)
+  # 
+  # boxLL <- spTransform(boxes, "+proj=longlat +ellps=WGS84")
+  # 
+  # writeOGR(boxLL, ".", "NGOM_LL", "ESRI Shapefile", overwrite_layer = TRUE)
+  # 
+  # ## re-create our original bgm? 
+  # ##system('java -jar bgmeriser-stripped.jar  -from "+proj=longlat +ellps=WGS84" -to  "+proj=utm +a=6378137.0 +es=0.006694380022900787 +lon_0=-81d00 +lat_0=0d00 +x_0=500000.0 +y_0=0.0 +k=0.9996 +zone=17" NGOM_LL.shp')
+  # 
+  
+  
