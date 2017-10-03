82714f335ed0e45fc3d7ccdb4795e2a4ca9e2f2e
Michael Sumner <mdsumner@gmail.com>
2017-10-03 04:00:54 +0000

    small changes to bgmfile

--- R/bgmfile.r
+++ R/bgmfile.r
@@ -16,6 +16,8 @@
 #' @importFrom dplyr %>% select distinct_ arrange bind_rows bind_cols distinct mutate mutate_ inner_join
 #' @importFrom tibble as_tibble tibble
 #' @importFrom utils  head type.convert
+#' @importFrom stringr str_split
+#' @importFrom readr read_file
 #' @examples 
 #' library(bgmfiles)
 #' bfile <- sample(bgmfiles(), 1L)
@@ -26,18 +28,22 @@ bgmfile <- function(x, ...) {
 
   if (nchar(x) < 1) stop("file path is empty string")
   if (!file.exists(x)) mess <- stop(sprintf("no file found '%s'\n", x))
-  tx <- readLines(x)  
+  #system.time(tx <- readLines(x)) 
+  tx <- stringr::str_split(readr::read_file(x), "\n")[[1]]
   nch <- length(tx)
   if (nch == 0) {
     stop(sprintf("no lines found in file %s", x))
   }
   ## all indexes
-  facesInd <- grep("^face", tx)
-  boxesInd <- grep("^box", tx)
-  bnd_vertInd <- grep("^bnd_vert", tx)
+#  facesInd <- grep("^face", tx)
+  facesInd <- which(stringr::str_detect(tx, "^face"))
+ # boxesInd <- grep("^box", tx)
+  boxesInd <- which(stringr::str_detect(tx, "^box"))
+#  bnd_vertInd <- grep("^bnd_vert", tx)
+  bnd_vertInd <- which(stringr::str_detect(tx, "^bnd_vert"))
   ## all comments
-  hashInd <- grep("^#", tx)
-  
+#  hashInd <- grep("^#", tx)
+ hashInd <- which(stringr::str_detect(tx, "^#")) 
   ## unique starting tokens
   ust <- sort(unique(unlist(lapply(strsplit(tx[-c(facesInd, boxesInd, bnd_vertInd, hashInd)], "\\s+"), "[", 1))))
   ust <- ust[nchar(ust) > 0]
@@ -58,15 +64,15 @@ bgmfile <- function(x, ...) {
   
   faceslist <- grepItems(tx[facesInd], "face", actual_numfaces)
   ## remove len, cs, lr from faceparse, all belong on the face not the face verts
-  faceverts <-  do.call(dplyr::bind_rows, lapply(seq_along(faceslist), function(xi) {a <- facevertsparse(faceslist[[xi]]); a$.fx0 <- xi - 1; a}))
-  faces <-   do.call(dplyr::bind_rows, lapply(seq_along(faceslist), function(xi) {a <-facedataparse(faceslist[[xi]]); a$.fx0 <- xi - 1; a}))
+  faceverts <-  dplyr::bind_rows(lapply(seq_along(faceslist), function(xi) {a <- facevertsparse(faceslist[[xi]]); a$.fx0 <- xi - 1; a}))
+  faces <-   dplyr::bind_rows(lapply(seq_along(faceslist), function(xi) {a <-facedataparse(faceslist[[xi]]); a$.fx0 <- xi - 1; a}))
   faces$label <- unlist(lapply(faceslist, function(x) strsplit(x[1], "\\.")[[1]][1]))
   
   boxeslist <- grepItems(tx[boxesInd], "box", as.numeric(extra["nbox"]))
   boxes0 <- lapply(seq_along(boxeslist), function(xi) {a <- boxparse(boxeslist[[xi]]); a$.bx0 <- xi - 1; a})
   ## we only need boxverts for non-face boxes (boundary faces), but use to check data sense
-  boxverts <- do.call(dplyr::bind_rows, lapply(seq_along(boxes0), function(xa) {aa <- boxes0[[xa]]$verts; .bx0 = rep(xa - 1, nrow(boxes0[[xa]]$verts)); aa$.bx0 <- .bx0; aa}))
-  boxes<- do.call(dplyr::bind_rows, 
+  boxverts <- dplyr::bind_rows(lapply(seq_along(boxes0), function(xa) {aa <- boxes0[[xa]]$verts; .bx0 = rep(xa - 1, nrow(boxes0[[xa]]$verts)); aa$.bx0 <- .bx0; aa}))
+  boxes<- dplyr::bind_rows( 
                   lapply(boxes0, function(a) dplyr::bind_cols(tibble::as_tibble(a[["meta"]]), 
                                                               tibble::as_tibble(a[c("insideX", "insideY", ".bx0")]))))
   ## ibox/iface are the component faces, and ibox the neighbouring box (.bx0 is the box we belong to!)
