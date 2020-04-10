221def3438542706dc96307312f62ef199dc530f
Michael Sumner <mdsumner@gmail.com>
2020-04-11 08:24:45 +1000

    avoid  sp  noise

--- tests/testthat/test-parse.R
+++ tests/testthat/test-parse.R
@@ -26,11 +26,13 @@ test_that("box parse handles different whitespace", {
 })
 
 test_that("fixproj can handle many weird proj.4 strings", {
-  expect_silent(sp::CRS(rbgm:::fixproj("+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")))
-  expect_silent(sp::CRS(rbgm:::fixproj("proj=aea lat_1=-18 lat_2=-36 lat_0=0 lon_0=134 x_0=3000000 y_0=6000000 ellps=GRS80 towgs84=0,0,0,0,0,0,0 units=m no_defs")))
-  expect_silent(sp::CRS(rbgm:::fixproj("+proj=longlat +a=6378137.0 +es=0.0066943799901413165 +lon_0=0d00 +lat_0=0d00 +x_0=0.0 +y_0=0.0")))
-  expect_silent(sp::CRS(rbgm:::fixproj("proj=utm + zone=55 + datum = WGS84 +units=m +no_defs")))
-  expect_silent(sp::CRS(rbgm:::fixproj("+proj=laea +lat_0=-63 +lon_0=82 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84")))
-  expect_silent(sp::CRS(rbgm:::fixproj("+proj=longlat +a=6378137.0 +es=0.0066943799901413165 +lon_0=0d00 +lat_0=0d00 +x_0=0.0 +y_0=0.0")))
-  expect_silent(sp::CRS(rbgm:::fixproj("proj=lcc lat_1=17.5 lat_2=29.5 lat_0=0 lon_0=-102.0 x_0=2000000.0 y_0=0 ellps=clrk66 datum=NAD27 units=m no_defs")))
+  expect_silent(
+    rbgm:::fixproj("+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"))
+  
+  expect_silent(rbgm:::fixproj("proj=aea lat_1=-18 lat_2=-36 lat_0=0 lon_0=134 x_0=3000000 y_0=6000000 ellps=GRS80 towgs84=0,0,0,0,0,0,0 units=m no_defs"))
+  expect_silent(rbgm:::fixproj("+proj=longlat +a=6378137.0 +es=0.0066943799901413165 +lon_0=0d00 +lat_0=0d00 +x_0=0.0 +y_0=0.0"))
+  expect_silent(rbgm:::fixproj("proj=utm + zone=55 + datum = WGS84 +units=m +no_defs"))
+  expect_silent(rbgm:::fixproj("+proj=laea +lat_0=-63 +lon_0=82 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84"))
+  expect_silent(rbgm:::fixproj("+proj=longlat +a=6378137.0 +es=0.0066943799901413165 +lon_0=0d00 +lat_0=0d00 +x_0=0.0 +y_0=0.0"))
+  expect_silent(rbgm:::fixproj("proj=lcc lat_1=17.5 lat_2=29.5 lat_0=0 lon_0=-102.0 x_0=2000000.0 y_0=0 ellps=clrk66 datum=NAD27 units=m no_defs"))
 })
